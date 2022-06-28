d2d2d632d429ea59328de654986a88198a38875c
Letale-vc <letalerv@gmail.com>
2022-06-28 21:25:02 +0300

    add test

--- src/service/service.controller.spec.ts
+++ src/service/service.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test } from '@nestjs/testing'
+import { create } from 'domain'
 import { UpdateResult } from 'typeorm'
+import { ServiceCreateDto } from './dto/service-create.dto'
 import { ServiceController } from './service.controller'
+import { Service } from './service.entity'
 import { ServiceService } from './service.service'
 
 describe('ServiceController', () => {
@@ -44,7 +47,7 @@ describe('ServiceController', () => {
   })
 
   describe('updateService', () => {
-    it('should be return result', async () => {
+    it('should be return result,  service.update: tobecalledwith upateobject+user.uuid, toBeCalledTimes 1 time ', async () => {
       const resultUpdate = { test: 'test' }
       const updateObj = {
         uuid: '321',
@@ -62,6 +65,38 @@ describe('ServiceController', () => {
 
       const res = await serviceController.updateService(user, updateObj)
       expect(res).toEqual(resultUpdate)
+      expect(service.update).toBeCalledWith(updateObj, user.uuid)
+      expect(service.update).toBeCalledTimes(1)
+    })
+  })
+
+  describe('createService', () => {
+    it('should be called function service.create, and must pass arguments createObj and user.uuid', async () => {
+      const createObj = {
+        bulk: 1,
+        leagueId: 1,
+        currencyId: 1,
+        price: 2,
+        sellOrBuy: 'WTB',
+        serviceNameId: 1
+      }
+
+      jest.spyOn(service, 'create').mockResolvedValue()
+      await serviceController.createService(user, createObj)
+      expect(service.create).toBeCalledTimes(1)
+      expect(service.create).toBeCalledWith(createObj, user.uuid)
+    })
+  })
+
+  describe('', () => {
+    it('should be return result, and service.find called times 1', async () => {
+      const searchParams = {
+        serviceNameId: 1,
+        leagueId: 1,
+        sellOrBuy: 'wtb'
+      }
+
+      const res = await serviceController.getServices(searchParams)
     })
   })
 })
